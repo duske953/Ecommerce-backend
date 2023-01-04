a92ff2102824ed6f597f081e975c7611e022a432
unknown <kennyduske@gmail.com>
2023-01-03 22:30:31 -0800

    updated authcontroller.js file

--- controller/authController.js
+++ controller/authController.js
@@ -21,7 +21,7 @@ function sendCookie(req, res, token) {
     keys,
   });
   return cookies.set("jwt", token, {
-    domain: ".vercel.app",
+    domain: ".eloho.herokuapp.com",
     sameSite: false,
     secure: true,
     path: "/",
