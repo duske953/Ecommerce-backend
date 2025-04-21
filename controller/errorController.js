588b3fd5526624f65290fd162635fcfcb9345ae8
Eloho Kennedy <kennyduske@gmail.com>
2025-04-21 11:17:31 +0100

    remove development errors from produciton

--- controller/errorController.js
+++ controller/errorController.js
@@ -26,7 +26,7 @@ function errorController(err, req, res, next) {
     if (error.expose === false)
       return res.status(errCode).json({
         status: error.status,
-        message: error.message,
+        message: "something went terribly wrong. That's all we know",
       });
     return res.status(errCode).json({
       status: error.status,
