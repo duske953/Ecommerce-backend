38390cb488c5f10295c3be18937de8923d625987
Eloho Kennedy <kennyduske@gmail.com>
2025-04-24 21:56:53 +0100

    remove dev errors

--- controller/errorController.js
+++ controller/errorController.js
@@ -26,7 +26,7 @@ function errorController(err, req, res, next) {
     if (error.expose === false)
       return res.status(errCode).json({
         status: error.status,
-        message: error.message,
+        message: 'something went wrong',
       });
     return res.status(errCode).json({
       status: error.status,
